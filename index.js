d79c4b56af27b12318b916ce1d734086e8579843
Shruti Koranne <shrutikoranne123@gmail.com>
2026-01-27 11:52:06 +0530

    commit on 27th Jan for working pipeline without SMTP

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end('ARM64 container running on AMD64 Ubuntu EC2');
+  res.end('ARM64 container running on AMD64 Ubuntu EC2 for SMTP notification');
 }).listen(3000);
